13f8f8eb1bb00ca4547c6677939b8ae2d4eac644
Ze Jin <zj58@en-cs-totient-01.coecis.cornell.edu>
2015-09-25 09:53:59 -0400

    update order + block

--- block/README.md
+++
@@ -1,6 +0,0 @@
-## Method 
-one layer of (4.1) blocking
-
-## Result
-best with block size = 64, 128, 256 
-
--- plot_b_jki.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd ~/matmul-
-
-./plotter.py basic b_jki_2 b_jki_4 b_jki_8 b_jki_16 b_jki_32 
-mv timing.pdf timing_b_jki_1.pdf
-
-./plotter.py basic b_jki_64 b_jki_128 b_jki_256 b_jki_512 b_jki_1024
-mv timing.pdf timing_b_jki_2.pdf
